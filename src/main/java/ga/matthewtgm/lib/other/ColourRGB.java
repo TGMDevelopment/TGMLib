39ceb7dd94b82cf81b839cba84754207babd8167
MatthewTGM <matthewtgm120@gmail.com>
2021-02-17 15:51:35 +0200

    Oops

---
+++ src/main/java/ga/matthewtgm/lib/other/ColourRGB.java
@@ -0,0 +1,63 @@
+package ga.matthewtgm.lib.other;
+
+import java.awt.*;
+
+public class ColourRGB {
+
+    private int r, g, b, a;
+
+    public ColourRGB(int r, int g, int b, int a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+
+    public ColourRGB(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = 255;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getRGB() {
+        return new Color(r, g, b).getRGB();
+    }
+
+    public int getRGBA() {
+        return new Color(r, g, b, a).getRGB();
+    }
+
+}
