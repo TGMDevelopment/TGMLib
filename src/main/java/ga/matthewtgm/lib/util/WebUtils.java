1796a8796413dbeaf468863c11ddbf88114db582
MatthewTGM <matthewtgm120@gmail.com>
2021-02-17 15:28:43 +0200

    v0.7.6

---
+++ src/main/java/ga/matthewtgm/lib/util/WebUtils.java
@@ -0,0 +1,29 @@
+package ga.matthewtgm.lib.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class WebUtils {
+
+    private static WebUtils INSTANCE;
+
+    public static WebUtils getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new WebUtils();
+        return INSTANCE;
+    }
+
+    public String getJsonOnline(String url) {
+        try {
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+            reader.lines().forEach(builder::append);
+            return builder.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
